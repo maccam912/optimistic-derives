f935ae6e91704259880b912817f684840608e166
maccam912 <maccam912@gmail.com>
2021-08-22 09:17:05 -0500

    time will tell if this was a bad idea

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,78 @@ extern crate syn;
 use proc_macro2::TokenStream;
 
 #[proc_macro_attribute]
-pub fn optimistic_std(_metadata: proc_macro::TokenStream, input: proc_macro::TokenStream)
-                 -> proc_macro::TokenStream {
+pub fn optimistic(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input: TokenStream = input.into();
     let output = quote! {
-        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Serialize, Deserialize)]
         #input
     };
     output.into()
 }
 
 #[proc_macro_attribute]
-pub fn optimistic_serde(_metadata: proc_macro::TokenStream, input: proc_macro::TokenStream)
-                 -> proc_macro::TokenStream {
+pub fn optimistic_no_c(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input: TokenStream = input.into();
     let output = quote! {
-        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Serialize, Deserialize)]
+        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Serialize, Deserialize)]
+        #input
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn optimistic_no_eo(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(Debug, PartialEq, PartialOrd, Hash, Clone, Copy, Serialize, Deserialize)]
+        #input
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn optimistic_no_ceo(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(Debug, PartialEq, PartialOrd, Hash, Clone, Serialize, Deserialize)]
+        #input
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn optimistic_no_h(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Serialize, Deserialize)]
+        #input
+    };
+    output.into()
+}
+
+#[proc_macro_attribute]
+pub fn optimistic_no_ceho(
+    _metadata: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+    let output = quote! {
+        #[derive(Debug, PartialEq, PartialOrd, Clone, Serialize, Deserialize)]
         #input
     };
     output.into()
